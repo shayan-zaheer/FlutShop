398a23e3944fc0f9a34545433078314ebdada382
Shayan Zaheer <139066765+shayan-zaheer@users.noreply.github.com>
2025-01-28 02:46:16 +0500

    Header done

---
+++ lib/home_page.dart
@@ -0,0 +1,101 @@
+import "package:flutter/material.dart";
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> filters = const ["All", "Adidas", "Nike", "Bata"];
+  late String selectedFilter;
+
+  @override
+  void initState(){
+    super.initState();
+    selectedFilter = filters[0];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Color.fromRGBO(225, 225, 225, 1),
+      ),
+      borderRadius: BorderRadius.horizontal(
+        left: Radius.circular(32),
+      ),
+    );
+
+    return Scaffold(
+        body: SafeArea(
+      // avoids the notch at the top or bottom
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  "Shoes\nCollection",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 35,
+                  ),
+                ),
+              ),
+              Expanded(
+                // take as much space as possible
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Search",
+                    prefixIcon: Icon(Icons.search),
+                    border: border,
+                    enabledBorder: border,
+                    focusedBorder: border,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 120,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+              itemCount: filters.length,
+              itemBuilder: (context, index){
+                  final filter = filters[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 8
+                    ),
+                    child: GestureDetector(
+                        onTap:(){
+                            selectedFilter = filter;
+                        },
+                      child: Chip(
+                          backgroundColor: selectedFilter == filter ? Theme.of(context).colorScheme.primary : Color.fromRGBO(245, 247, 249, 1), // Theme.of(context) works in inheritance, it goes up in parent widgets until it finds a nearest themewidget (MaterialApp)
+                          labelStyle: TextStyle(
+                              fontSize: 16,
+                          ),
+                          label: Text(filter),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 20,
+                              vertical: 15,
+                          ),
+                          side: BorderSide.none,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50),
+                          )
+                      ),
+                    ),
+                  );
+              }
+            ),
+          )
+        ],
+      ),
+    ),);
+  }
+}
