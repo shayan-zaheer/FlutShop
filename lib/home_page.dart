f1faa950c868ae47bbaea9ee7d56937688d85c01
Shayan Zaheer <139066765+shayan-zaheer@users.noreply.github.com>
2025-01-30 14:31:42 +0500

    Added images from assets

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,6 @@
+import "package:flutshop/product_card.dart";
 import "package:flutter/material.dart";
+import "package:flutshop/global_variables.dart";
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,7 +14,7 @@ class _HomePageState extends State<HomePage> {
   late String selectedFilter;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     selectedFilter = filters[0];
   }
@@ -29,73 +31,87 @@ class _HomePageState extends State<HomePage> {
     );
 
     return Scaffold(
-        body: SafeArea(
-      // avoids the notch at the top or bottom
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Text(
-                  "Shoes\nCollection",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 35,
-                  ),
-                ),
-              ),
-              Expanded(
-                // take as much space as possible
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Search",
-                    prefixIcon: Icon(Icons.search),
-                    border: border,
-                    enabledBorder: border,
-                    focusedBorder: border,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 120,
-            child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-              itemCount: filters.length,
-              itemBuilder: (context, index){
-                  final filter = filters[index];
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 8
+      body: SafeArea(
+        // avoids the notch at the top or bottom
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(
+                    "Shoes\nCollection",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35,
                     ),
-                    child: GestureDetector(
-                        onTap:(){
+                  ),
+                ),
+                Expanded(
+                  // take as much space as possible
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Search",
+                      prefixIcon: Icon(Icons.search),
+                      border: border,
+                      enabledBorder: border,
+                      focusedBorder: border,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 120,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: filters.length,
+                  itemBuilder: (context, index) {
+                    final filter = filters[index];
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 8,
+                      ),
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
                             selectedFilter = filter;
+                          });
                         },
-                      child: Chip(
-                          backgroundColor: selectedFilter == filter ? Theme.of(context).colorScheme.primary : Color.fromRGBO(245, 247, 249, 1), // Theme.of(context) works in inheritance, it goes up in parent widgets until it finds a nearest themewidget (MaterialApp)
+                        child: Chip(
+                          backgroundColor: selectedFilter == filter
+                              ? Theme.of(context).colorScheme.primary
+                              : Color.fromRGBO(245, 247, 249,
+                                  1), // Theme.of(context) works in inheritance, it goes up in parent widgets until it finds a nearest themewidget (MaterialApp)
                           labelStyle: TextStyle(
-                              fontSize: 16,
+                            fontSize: 16,
                           ),
                           label: Text(filter),
                           padding: EdgeInsets.symmetric(
-                              horizontal: 20,
-                              vertical: 15,
+                            horizontal: 20,
+                            vertical: 15,
                           ),
                           side: BorderSide.none,
                           shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(50),
-                          )
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                        ),
                       ),
-                    ),
-                  );
-              }
+                    );
+                  }),
             ),
-          )
-        ],
+            Expanded(
+              child: ListView.builder(
+                  itemCount: products.length,
+                  itemBuilder: (context, index){
+                      final product = products[index];
+                      return ProductCard(title: product["title"] as String, price: product["price"] as double, image: product["imageUrl"] as String);
+                  }
+              ),
+            )
+          ],
+        ),
       ),
-    ),);
+    );
   }
 }
---
+++ lib/product_card.dart
@@ -0,0 +1,27 @@
+import "package:flutter/material.dart";
+
+class ProductCard extends StatelessWidget {
+    final String title;
+    final double price;
+    final String image;
+
+    const ProductCard({super.key, required this.title, required this.price, required this.image});
+
+    @override
+    Widget build(BuildContext context){
+        return Container(
+            color: Color.fromRGBO(216, 240, 253, 1),
+            child: Column(
+                children: [
+                    Text(title),
+                    SizedBox(height: 5),
+                    Text("\$$price"),
+                    SizedBox(height: 5),
+                    Image(
+                        image: AssetImage(image),
+                    )
+                ],
+            )
+        );
+    }
+}
